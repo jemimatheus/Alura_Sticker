714092c3f204eb21f94ce2002762d079568b7e67
Jemima <jemima.ramoscampanha@gmail.com>
2022-07-21 18:06:26 -0300

    Adicionado Abstração do InputStream

--- src/App.java
+++ src/App.java
@@ -1,4 +1,6 @@
+import java.io.InputStream;
 import java.net.URI;
+import java.net.URL;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
@@ -30,9 +32,21 @@ public class App {
         //Exibir os dados extraidos
 
         for (Map<String,String> filme : listaDeFilmes) {
-            System.out.println(filme.get("title"));
-            System.out.println(filme.get("image"));
-            System.out.println(filme.get("imDbRating"));
+            
+            String urlImages = filme.get("image");
+            String titulo = filme.get("title");
+            
+            InputStream inputStream = new URL(urlImages).openStream();
+
+            String nomeArquivo = titulo + ".png";
+
+            var geradora = new GeradorDeFigurinhas();
+            geradora.cria(inputStream, nomeArquivo);
+
+    
+            System.out.println(filme.get(titulo));
+            System.out.println();
         }
     }
 }
+ 
